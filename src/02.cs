080c3ab509d8e21bbf33480644f643ca37672060
jonathansim94 <33371255+jonathansim94@users.noreply.github.com>
2021-05-07 23:57:49 +0200

    cipher test

--- src/02.cs
+++ src/02.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+using System.Threading;
 
 namespace ZeroTwo.src {
     class _02 {
         public static void Main(string[] args) {
             Unp();
+        }
+
+        public static char cipher(char ch, int key) {
+            if (!char.IsLetter(ch)) {
+                return ch;
+            }
+            char d = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((((ch + key) - d) % 26) + d);
+        }
+
+        private static string Encipher(string input, int key) {
+            string output = string.Empty;
+
+            foreach (char ch in input)
+                output += cipher(ch, key);
+
+            return output;
+        }
+
+        private static string Decipher(string input, int key) {
+            return Encipher(input, 26 - key);
         }
 
         private static void Unp() {
@@ -14,7 +36,7 @@ namespace ZeroTwo.src {
             using (Stream stream = assembly.GetManifestResourceStream(res)) {
                 using (StreamReader reader = new StreamReader(stream)) {
                     string core = reader.ReadToEnd();
-                    Byte[] bytes = Convert.FromBase64String(core);
+                    Byte[] bytes = Convert.FromBase64String(Decipher(core, 5));
                     var loaded = Assembly.Load(bytes);
                     var entryPoint = loaded.EntryPoint;
                     var commandArgs = new string[0];
--- src/Packer.cs
+++ src/Packer.cs
@@ -9,11 +9,33 @@ namespace ZeroTwo.src {
             Pack();
         }
 
+        public static char cipher(char ch, int key) {
+            if (!char.IsLetter(ch)) {
+                return ch;
+            }
+            char d = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((((ch + key) - d) % 26) + d);
+        }
+
+        private static string Encipher(string input, int key) {
+            string output = string.Empty;
+
+            foreach (char ch in input)
+                output += cipher(ch, key);
+
+            return output;
+        }
+
+        private static string Decipher(string input, int key) {
+            return Encipher(input, 26 - key);
+        }
+
         private static void Pack() {
             Byte[] bytes = File.ReadAllBytes(EXE);
             String enc = Convert.ToBase64String(bytes);
             using (StreamWriter sw = File.CreateText(RES)) {
-                sw.Write(enc);
+                string cipherText = Encipher(enc, 5);
+                sw.Write(cipherText);
             }
         }
 
