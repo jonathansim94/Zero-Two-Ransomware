08bb4c99d39ecfbcfb1ddd21c952a778244c5c4d
jonathansim94 <33371255+jonathansim94@users.noreply.github.com>
2021-05-10 17:48:19 +0200

    working unpacker

--- src/02.cs
+++ src/02.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
@@ -9,46 +8,57 @@ namespace ZeroTwo.src {
         private static byte[] k = Encoding.ASCII.GetBytes("0202020202020202");
         private static byte[] s = Encoding.ASCII.GetBytes("0202020202020202");
 
+        private const int EXEC_FLAG = 1;
+
         public static void Main(string[] args) {
-            Unp();
+            if (args.Length == 0)
+                Go(Unp(), false);
+            else if (int.Parse(args[0]) == EXEC_FLAG) {
+                Go(Unp(), true);
+            }
         }
 
-        private static void Unp() {
+        private static MemoryStream Unp() {
             var assembly = Assembly.GetExecutingAssembly();
-            var res = "ZeroTwo.src.core.Core";
+            var res = "ZeroTwo.src.core.EncExe";
             using (Stream stream = assembly.GetManifestResourceStream(res)) {
-                using (StreamReader reader = new StreamReader(stream)) {
-                    string core = reader.ReadToEnd();
-                    byte[] bytes = Convert.FromBase64String(core);
-                    Stream bytesStream = new MemoryStream(bytes);
 
-                    RijndaelManaged AES = new RijndaelManaged();
-                    AES.KeySize = 256;
-                    AES.BlockSize = 128;
+                RijndaelManaged AES = new RijndaelManaged();
+                AES.KeySize = 256;
+                AES.BlockSize = 128;
 
-                    var key = new Rfc2898DeriveBytes(k, s, 1000);
-                    AES.Key = key.GetBytes(AES.KeySize / 8);
-                    AES.IV = key.GetBytes(AES.BlockSize / 8);
-                    AES.Padding = PaddingMode.Zeros;
-                    AES.Mode = CipherMode.CBC;
+                var key = new Rfc2898DeriveBytes(k, s, 1000);
+                AES.Key = key.GetBytes(AES.KeySize / 8);
+                AES.IV = key.GetBytes(AES.BlockSize / 8);
+                AES.Padding = PaddingMode.Zeros;
+                AES.Mode = CipherMode.CBC;
 
-                    CryptoStream cs = new CryptoStream(bytesStream, AES.CreateDecryptor(), CryptoStreamMode.Read);
-                    MemoryStream finalBytesStream = new MemoryStream();
+                CryptoStream cs = new CryptoStream(stream, AES.CreateDecryptor(), CryptoStreamMode.Read);
+                MemoryStream finalBytesStream = new MemoryStream();
 
-                    int data;
-                    while ((data = cs.ReadByte()) != -1)
-                        finalBytesStream.WriteByte((byte)data);
+                int data;
+                while ((data = cs.ReadByte()) != -1)
+                    finalBytesStream.WriteByte((byte)data);
 
-                    cs.Close();
+                cs.Close();
 
-                    //File.Delete(inputFile);
+                //File.Delete(inputFile);
 
-                    var loaded = Assembly.Load(finalBytesStream.ToArray());
-                    var entryPoint = loaded.EntryPoint;
-                    var commandArgs = new string[0];
-                    var returnValue = entryPoint.Invoke(null, new object[] { commandArgs });
-                }
+                return finalBytesStream;
             }
+        }
+
+        private static void Go(MemoryStream ms, bool fatality) {
+            var loaded = Assembly.Load(ms.ToArray());
+            var entryPoint = loaded.EntryPoint;
+
+            string[] commandArgs;
+            if (fatality) {
+                commandArgs = new string[1] { EXEC_FLAG.ToString() };
+            } else {
+                commandArgs = new string[0];
+            }
+            var returnValue = entryPoint.Invoke(null, new object[] { commandArgs });
         }
     }
 
