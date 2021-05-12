0e16d432cdd100f4f0c23d2ac4a54820b79ae95f
jonathansim94 <33371255+jonathansim94@users.noreply.github.com>
2021-05-12 18:11:31 +0200

    text filler cause bin file triggers defender

--- filler/fillerGen.py
+++ filler/fillerGen.py
@@ -1,10 +1,12 @@
-import os, sys
+import os, sys, random, string
 
-def generate_random_bin_file(filename,size_mb):
+def generate_random_txt_file(filename,size_mb):
     size = 1024 * 1024 * size_mb
-    with open('%s'%filename, 'wb') as fout:
-        fout.write(os.urandom(size))
-    pass
+    chars = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(size)])  # 1
+
+    with open(filename, 'w') as f:
+        f.write(chars)
 
 if __name__ == '__main__':
-    generate_random_bin_file("fill.02",int(sys.argv[1]))
+    generate_random_txt_file("fill.txt",int(sys.argv[1]))
+
