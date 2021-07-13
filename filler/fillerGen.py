13efa04c37ed11ab4e37c8b1b65f0ab10215e423
jonathansim94 <33371255+jonathansim94@users.noreply.github.com>
2021-07-13 17:35:11 +0200

    removed comment

--- filler/fillerGen.py
+++ filler/fillerGen.py
@@ -2,7 +2,7 @@ import os, sys, random, string
 
 def generate_random_txt_file(filename,size_mb):
     size = 1024 * 1024 * size_mb
-    chars = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(size)])  # 1
+    chars = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(size)])
 
     with open(filename, 'w') as f:
         f.write(chars)
